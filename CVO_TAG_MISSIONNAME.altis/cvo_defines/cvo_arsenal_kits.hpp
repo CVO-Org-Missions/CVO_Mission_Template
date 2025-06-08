8987e4910e238c88dd9056afb059e1937ba2fa23
OverlordZorn <dennisAzorn@gmail.com>
2025-06-08 14:19:52 +0200

    Update cvo_arsenal_kits.hpp

--- CVO_TAG_MISSIONNAME.altis/cvo_defines/cvo_arsenal_kits.hpp
+++ CVO_TAG_MISSIONNAME.altis/cvo_defines/cvo_arsenal_kits.hpp
@@ -1,26 +1,9 @@
 /*
 *   CVO Arsenal Define File
-*   This file will handle everything needed for the CVO Arsenal System.
-*
-* # General Attributes:
-*   editor_layer_name       <STRING>                Provide an Eden Editor layer name. All Objects inside this Layer will be made into CVO Arsenal Objects.
-*   object_variable_names   <ARRAY of STRINGS>      Provide the variable names of individual objects to make them into CVO Arsenal Objects.
-
-* # Kits
-*   In general, a Kit is a collection of items.
-*   Optionally, some strings can be attached to it - For example: Addon dependency or a condition.
-* 
-* ## Kit Attributes:
-*   addon_dependency     <STRING>   Name of an Addon - Will Check if the Addon is currently loaded. <Default: ""> Empty String will skip check.             Example: addon_dependency = "cigs_core";
-*   role                 <STRING>   Name/Identifyer of the Role. case-unsensitive.                  <Default: ""> Empty String will never get applied.      Example: role = "Machinegunner";
-*   id64                 <STRING>   steamID64 - getPlayerUID                                        <Default: ""> Empty String will never get applied.      Example: id64 = "123123123123";
-*   condition            <STRING>   Code as String, needs to return boolean.                        <Default: ""> Empty String will skip check.             Example: condition = "missionNamespace getVariable ["cvo_nightFight", false]";
-*   code                 <STRING>   Code as String, needs to return array of classnames.            <Default: ""> Empty String will skip execution.         Example: code = "if (1 == 1) then { ["ace_banana"] } else { [] }";
-*   items <CLASS with SUBCLASSES>                                                                                                                           Example: class items {
-*   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                                                                                                                   |     class ace_banana;
-*   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~                                                                                                                                   | };
-*
+*   Detailed Information: https://github.com/CVO-Org/CVO-Auxiliary/blob/main/addons/arsenal/readme.md
+*   Example File: https://github.com/CVO-Org/CVO-Auxiliary/blob/main/.hemtt/missions/Arsenal.Altis/cvo_arsenal_kits.hpp
 */
+
 
 class cvo_arsenal_kits
 {
