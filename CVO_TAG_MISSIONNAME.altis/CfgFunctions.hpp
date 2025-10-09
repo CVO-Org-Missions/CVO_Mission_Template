2065df1c468e1a15fd56ddd07a49e1353474482a
OverlordZorn <dennisAzorn@gmail.com>
2025-10-09 21:53:20 +0200

    update default function folders

--- CVO_TAG_MISSIONNAME.altis/CfgFunctions.hpp
+++ CVO_TAG_MISSIONNAME.altis/CfgFunctions.hpp
@@ -3,12 +3,16 @@ class CfgFunctions {
     class mission // TAG
     {
 
-        class Mission // Category
+        class Init // Category
         {
-            class diary { preInit = 1; };
-            class babel_init { postInit = 1; }
+            class init_diary { preInit = 1; };
+            class init_babel { postInit = 1; }
         };
         class Common // Category
+        {
+            // class example {};
+        };
+        class Mission // Category
         {
             // class example {};
         };
